c352405a6182f745601327367a146c3f16c9d3da
Fletcher Foti <fscottfoti@gmail.com>
2016-08-07 00:48:09 +0100

    trying out medius blog template

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,10 +4,9 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Fletcher Foti'
 SITENAME = u'OaklandAnalytics'
-SITEURL = 'https://fscottfoti.github.io/blog'
+SITEURL = ''
 
-THEME="pelican-bootstrap3"
-BOOTSTRAP_THEME="united"
+THEME="./medius"
 
 PATH = 'content'
 
@@ -34,3 +33,29 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+DISPLAY_CATEGORIES_ON_MENU = True
+DISPLAY_PAGES_ON_MENU = True
+
+
+MEDIUS_CATEGORIES = {
+    'Category': {
+        'description': 'A galaxy is a gravitationally bound system of stars, stellar remnants, interstellar gas and dust, and dark matter.',
+        #'logo': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/GalacticRotation2.svg/250px-GalacticRotation2.svg.png',
+        'thumbnail': 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/NGC_3923_Elliptical_Shell_Galaxy.jpg/220px-NGC_3923_Elliptical_Shell_Galaxy.jpg'
+    }
+}
+
+
+MEDIUS_AUTHORS = {
+    'Onur Aslan': {
+        'description': """
+            And where does the newborn go from here? The net is vast and infinite.
+        """,
+        'cover': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Milky_Way_Arch.jpg/1920px-Milky_Way_Arch.jpg',
+        'image': 'https://lh6.googleusercontent.com/-zEMaXmWAhdI/AAAAAAAAAAI/AAAAAAAAAAA/eVdgsm3TIDU/s128-c-k/photo.jpg',
+        'links': (('github-square', 'https://github.com/onuraslan'),
+                  ('twitter-square', 'https://twitter.com/oasln'),
+                  ('envelope-square', '#')),
+    }
+}
