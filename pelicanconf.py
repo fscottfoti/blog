c2241ab091f8c3b345ca1a00b70b6b04265383ce
Fletcher Foti <fscottfoti@gmail.com>
2016-09-04 13:14:37 -0700

    added back CNAME

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = u'Fletcher Foti'
 SITENAME = u'OaklandAnalytics'
 SITEURL = 'http://localhost:8000'
-#SITEURL = "http://oaklandanalytics.com"
+SITEURL = "http://oaklandanalytics.com"
 
 THEME="./medius"
 
